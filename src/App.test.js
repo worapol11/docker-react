25e0f8346902b95c032e85ee5fe9d623904c2790
Worapol Worakunsap <worapolw@ais.co.th>
2021-10-27 15:57:53 +0700

    Initial Commit.

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-test('renders learn react link', () => {
+it('renders learn react link', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App/>, div);
+  ReactDOM.unmountComponentAtNode(div);
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
+});
+
+it('renders learn react link2', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
